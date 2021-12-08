576c0e159facbe1249f8e71c6213265512b5b713
jdayment <52637666+jdayment@users.noreply.github.com>
2021-12-08 14:28:48 -0700

    Delete FlexCard CPE/FlexCard CPE directory

--- FlexCard CPE/FlexCard CPE/force-app/main/default/lwc/sliderScreenComponent/sliderScreenComponent.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class LightningExampleSliderBasic extends LightningElement {
-    @api value;
-    @api max;
-    @api min;
-    @api label;
-}
