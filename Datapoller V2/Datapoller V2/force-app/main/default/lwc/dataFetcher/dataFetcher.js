f9c687ba1b4678b579d69626ab1b5d733dd7fc9a
jdayment <52637666+jdayment@users.noreply.github.com>
2023-01-25 08:58:31 -0700

    Add files via upload

---
+++ Datapoller V2/Datapoller V2/force-app/main/default/lwc/dataFetcher/dataFetcher.js
@@ -0,0 +1,49 @@
+import { api, LightningElement } from 'lwc';
+import getSobjects from '@salesforce/apex/DataFetcherController.getSobjects';
+import { FlowAttributeChangeEvent } from 'lightning/flowSupport';
+export default class DataFetcher extends LightningElement {
+    @api queryString;
+    @api retrievedRecords = [];
+    @api error;
+
+    //using a renderedCallback vs connectedCallback 
+    renderedCallback() {
+        //I needed to look for an empty string vs null on query string because it wasn't treating it as null for some reason
+        if(this.queryString != ""){
+            getSobjects({queryString : this.queryString}).then(
+                result => {
+                    
+                    this.retrievedRecords = result;
+                    this.dispatchEvent(new FlowAttributeChangeEvent('retrievedRecords', this.retrievedRecords));             
+                }
+            ).catch(
+                error => {
+                    this.error = error.body.message;
+                    console.error('error', error);
+                    this.dispatchEvent(new FlowAttributeChangeEvent('error', this.error.body.message));
+                    
+                }
+            );}
+        }
+
+    getRecords() {
+        {
+        
+            getSobjects({queryString : this.queryString}).then(
+                result => {
+                    
+                    this.retrievedRecords = result;
+                    this.dispatchEvent(new FlowAttributeChangeEvent('retrievedRecords', this.retrievedRecords));
+                                 
+                }
+            ).catch(
+                error => {
+                    this.error = error.body.message;
+                    console.error('error', error);
+                    this.dispatchEvent(new FlowAttributeChangeEvent('error', this.error.body.message)); 
+                }
+            );
+        }
+    }
+
+}
