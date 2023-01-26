38148a9c555317c538b0e737e63289910f6df6b1
jdayment <52637666+jdayment@users.noreply.github.com>
2023-01-26 08:14:50 -0700

    Update dataFetcher.js

--- Datapoller V2/Datapoller V2/force-app/main/default/lwc/dataFetcher/dataFetcher.js
+++ Datapoller V2/Datapoller V2/force-app/main/default/lwc/dataFetcher/dataFetcher.js
@@ -6,24 +6,11 @@ export default class DataFetcher extends LightningElement {
     @api retrievedRecords = [];
     @api error;
 
-    //using a renderedCallback vs connectedCallback 
+    //using a renderedCallback vs connectedCallback seems to work when using reactivity
     renderedCallback() {
         //I needed to look for an empty string vs null on query string because it wasn't treating it as null for some reason
         if(this.queryString != ""){
-            getSobjects({queryString : this.queryString}).then(
-                result => {
-                    
-                    this.retrievedRecords = result;
-                    this.dispatchEvent(new FlowAttributeChangeEvent('retrievedRecords', this.retrievedRecords));             
-                }
-            ).catch(
-                error => {
-                    this.error = error.body.message;
-                    console.error('error', error);
-                    this.dispatchEvent(new FlowAttributeChangeEvent('error', this.error.body.message));
-                    
-                }
-            );}
+            this.getRecords()}
         }
 
     getRecords() {
